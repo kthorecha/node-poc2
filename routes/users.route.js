fc0e16c10395cfaccf57853c08e9770da7d402f8
karan thorecha <kthorecha@gmail.com>
2021-06-24 13:57:17 +0530

    update

--- routes/users.route.js
+++ routes/users.route.js
@@ -5,7 +5,5 @@ const userController = require('../controllers/users.controller');
 /* GET users listing. */
 router.get('/', userController.getAll);
 router.post('/create', userController.create);
-router.put('/update/:id', userController.update);
-router.delete('/delete/:id', userController.delete);
 
 module.exports = router;
